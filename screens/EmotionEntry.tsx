593267322c6c8432dd5f5adc73a5593b76c47696
Or Cherfas <ocherfas@gmail.com>
2021-10-11 20:16:28 +0300

    scroll view for emotion entry

--- screens/EmotionEntry.tsx
+++ screens/EmotionEntry.tsx
@@ -1,23 +1,34 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Platform, ScrollView, StyleSheet } from 'react-native';
 import { View, Text } from '../components/Themed';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types';
+import { StatusBar } from 'expo-status-bar';
+import { useLayout } from '@react-native-community/hooks';
 
 export default function EmotionEntry({ route }: NativeStackScreenProps<RootStackParamList, 'EmotionEntry'>) {
+    const {onLayout, ...layout} = useLayout();
+
   return (
-    <View style={styles.container}>
-        <Text style={styles.title}>Event:</Text>
-        <Text style={styles.text}>{route.params.event}</Text>
-        <Text style={styles.title}>Emotion:</Text>
-        <Text style={styles.text}>{route.params.emotion}</Text>
+    <View onLayout={onLayout} style={styles.screen}>
+        <ScrollView contentContainerStyle={styles.container} style={{height: layout.height}}>
+            <Text style={styles.title}>Event:</Text>
+            <Text style={styles.text}>{route.params.event}</Text>
+            <Text style={styles.title}>Emotion:</Text>
+            <Text style={styles.text}>{route.params.emotion}</Text>
+        </ScrollView>
+        <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    screen: {
+        backgroundColor: "#fff",
         flex: 1,
+        width: "100%"
+      },
+    container: {
         padding: 30
     },
     title: {
